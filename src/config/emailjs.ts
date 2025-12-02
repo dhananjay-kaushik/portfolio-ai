045dc56a9aafa0a812ab8968fd6e12258ba1e135
Dhananjay Kaushik <dhananjaykaushik96@gmail.com>
2025-12-02 15:42:06 +0530

    Sixth Commit

--- src/config/emailjs.ts
+++ src/config/emailjs.ts
@@ -1,10 +1,10 @@
 export const emailjsConfig = {
     // Your EmailJS Service ID (from Integration page)
-    serviceId: "service_a2oz93j",
+    serviceId: process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || "",
 
     // Your EmailJS Template ID (from Email Templates page)
-    templateId: "template_p6oszvr",
+    templateId: process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || "",
 
     // Your EmailJS Public Key (from Account > API Keys)
-    publicKey: "lWubWqw7gb8SbPjRD",
+    publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY || "",
 };
